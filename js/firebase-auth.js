640a75b1fa88e0141460c37053da77f4fd1c2e28
Balaji Tippireddy <tippireddybalaji@gmail.com>
2026-05-09 17:36:19 +0530

    Update firebase-auth.js

--- js/firebase-auth.js
+++ js/firebase-auth.js
@@ -4,7 +4,7 @@
 
 const FireAuth = (() => {
   const firebaseConfig = {
-    apiKey: "IzaSyDvApXo4pS1PAFYSHlQss90v_F075CHJBg",
+    apiKey: "AIzaSyDvApXo4pS1PAFYSHlQss90v_F075CHJBg",
     authDomain: "takshashila-attendance.firebaseapp.com",
     projectId: "takshashila-attendance",
     storageBucket: "takshashila-attendance.firebasestorage.app",
